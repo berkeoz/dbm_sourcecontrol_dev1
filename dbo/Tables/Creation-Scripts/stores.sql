20d4fe7bf7ed52472aa610f07c15045601768768
berkeoz <email>
2022-07-05 00:57:53 +0000

    Store Creation

---
+++ dbo/Tables/Creation-Scripts/stores.sql
@@ -0,0 +1,10 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[stores](
+	[store_name] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[store_address] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+
+GO
