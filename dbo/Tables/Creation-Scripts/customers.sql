28083a0157ead84a4255ff46fcc7d406ce024425
berkeoz <email>
2022-07-05 00:45:47 +0000

    Customer is created

---
+++ dbo/Tables/Creation-Scripts/customers.sql
@@ -0,0 +1,11 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[customers](
+	[names] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[surnames] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[addresses] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+
+GO
