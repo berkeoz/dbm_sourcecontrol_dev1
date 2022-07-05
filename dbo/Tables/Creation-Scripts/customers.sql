322466a0213a35d2dce28807fddfc892b84d675b
berkeoz <email>
2022-07-05 01:37:13 +0000

    feature_1

--- dbo/Tables/Creation-Scripts/customers.sql
+++ dbo/Tables/Creation-Scripts/customers.sql
@@ -5,7 +5,8 @@ GO
 CREATE TABLE [dbo].[customers](
 	[names] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[surnames] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[addresses] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+	[addresses] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[middlename] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 
 GO
---
+++ dbo/Tables/Creation-Scripts/geographies.sql
@@ -0,0 +1,9 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[geographies](
+	[area_name] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+
+GO
